801dff6475b9abfa174f7d651744db26d489ea6a
Gerald Oliva <geraldsamoliva@gmail.com>
2020-06-06 23:55:35 -0400

    Post model

---
+++ models/Post.js
@@ -0,0 +1,48 @@
+const postCollection = require('../db').db().collection("posts")
+const ObjectID = require('mongodb').ObjectID
+
+let Post = function(data, userid) {
+    this.data = data
+    this.errors = []
+    this.userid = userid
+}
+
+Post.prototype.cleanUp = function() {
+    if (typeof(this.data.title) != "string") {this.data.title = ""}
+    if (typeof(this.data.body) != "string") {this.data.body = ""}
+
+    // Get rid of any bogus properties
+    this.data = {
+        title: this.data.title.trim(),
+        body: this.data.body.trim(),
+        author: ObjectID(this.userid),
+        createdDate: new Date()
+    }
+}
+
+
+
+Post.prototype.validate = function () {
+    if (this.data.title == "") {this.errors.push("You must provide a title")}
+    if (this.data.body == "") {this.errors.push("You must provide post content")}
+}
+
+Post.prototype.create = function () {
+    return new Promise((resolve, reject) => {
+        this.cleanUp()
+        this.validate()
+        if (!this.errors.length) {
+            // Save post into database
+            postCollection.insertOne(this.data).then(() => {
+                resolve()
+            }).catch(() => {
+                this.errors.push("Please try again later.")
+                reject(this.errors)
+            })
+        } else {
+            reject(this.errors)
+        }
+    })
+}
+
+module.exports = Post
