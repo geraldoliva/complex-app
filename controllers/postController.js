ca35536e412e0946dfdb818421a0726ec35584f6
Gerald Oliva <geraldsamoliva@gmail.com>
2020-07-16 23:50:18 -0400

    Add delete post functionality

--- controllers/postController.js
+++ controllers/postController.js
@@ -24,17 +24,17 @@ exports.viewSingle = async function(req, res) {
     }
 }
 
-exports.viewEditScreen = async function(req, res) {
+exports.viewEditScreen = async function (req, res) {
     try {
-        let post = await Post.findSingleById(req.params.id)
-        if (post.authorId == req.visitorId) {
-            res.render('edit-post', { post: post })
+        let post = await Post.findSingleById(req.params.id, req.visitorId)
+        if (post.isVisitorOwner) {
+            res.render("edit-post", { post: post })
         } else {
-            req.flash("errors", "You do not have permission to perform that action")
+            req.flash("errors", "You do not have permission to perform that action.")
             req.session.save(() => res.redirect("/"))
         }
-    } catch (error) {
-        res.render('404')
+    } catch {
+        res.render("404")
     }
 }
 
@@ -64,5 +64,15 @@ exports.edit = function(req, res) {
          req.session.save(function() {
              res.redirect("/")
          })
+    })
+}
+
+exports.delete = function(req, res) {
+    Post.delete(req.params.id, req.visitorId).then(() => {
+        req.flash("success", "Post successfully deleted.")
+        req.session.save(() => res.redirect(`/profile/${req.session.user.username}`))
+    }).catch(() => {
+        req.flash("errors", "You do not have permission to perform that action.")
+        req.session.save(() => res.redirect('/'))
     })
 }
