7deb7f26f0d05035a4d7f4cb55159bcfe3454ff6
Gerald Oliva <geraldsamoliva@gmail.com>
2020-07-19 15:41:16 -0400

    Add follow functionality

--- router.js
+++ router.js
@@ -12,6 +12,9 @@ router.post('/logout', userController.logout)
 
 // Profile related routes
 router.get('/profile/:username', userController.ifUserExists, userController.sharedProfileData, userController.profilePostsScreen)
+router.get('/profile/:username/followers', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowersScreen)
+router.get('/profile/:username/following', userController.ifUserExists, userController.sharedProfileData, userController.profileFollowingScreen)
+
 
 // Post related routes
 router.get('/create-post', userController.mustBeLoggedIn, postController.viewCreateScreen)
